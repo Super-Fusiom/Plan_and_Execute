d005f23e062768f675a9ee6076c45a9953630042
Paul <paul21479@gmail.com>
2023-06-07 10:39:22 +1200

    Made a window with a button and label using a class

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import sqlite3
-from tkinter import *
+import tkinter
 from tkinter import messagebox
 from tkinter import ttk
 
-class Window:
-    def __init__(self, window):
-        self.window = window
+class App(tkinter.Tk):
+    def __init__(self):
+        super().__init__()
 
+        self.title("Plan and Execute")
+        self.geometry("500x500")
 
-main_window = Window("edger")
+        self.label = ttk.Label(self, text="PLAN")
+        self.label.pack()
 
-print(main_window.window)
+        self.button = ttk.Button(self, text="Execute")
+        self.button['command'] = self.button_clicked
+        self.button.pack()
+
+    def button_clicked(self):
+        print("It works")
+            
+
+if __name__ == "__main__":
+    main_window = App()
+    main_window.mainloop()
